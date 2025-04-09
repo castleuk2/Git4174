880dd8318532b0b88ddc7355938eb8c722f59193
castleuk2 <wham63@naver.com>
2025-04-09 14:06:37 +0000

    Update mynumber.c to print odd and even numbers

--- mynumber.c
+++ mynumber.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
-int main(){
-	for(int i = 1; i <= 100; i++){
-		if(i % 2 != 0){
-			printf("%d\n", i);
-		}
-	}
-	return 0;
+
+int main() {
+    // 홀수 출력
+    printf("Odd numbers from 1 to 100:\n");
+    for (int i = 1; i <= 100; i++) {
+        if (i % 2 != 0) {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n\n");
+
+    // 짝수 출력
+    printf("Even numbers from 1 to 100:\n");
+    for (int i = 1; i <= 100; i++) {
+        if (i % 2 == 0) {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");
+    return 0;
 }
+
